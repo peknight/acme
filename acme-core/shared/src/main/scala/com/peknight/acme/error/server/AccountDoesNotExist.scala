38c0adbe203c323063a8ef74d5142fb091a78957
peknight <JKpeknight@gmail.com>
2025-02-13 18:13:20 +0800

    update

--- acme-core/shared/src/main/scala/com/peknight/acme/error/server/AccountDoesNotExist.scala
+++ acme-core/shared/src/main/scala/com/peknight/acme/error/server/AccountDoesNotExist.scala
@@ -1,7 +1,13 @@
 package com.peknight.acme.error.server
 
+import cats.Monad
 import com.peknight.acme.identifier.Identifier
 import com.peknight.codec.circe.Ext
+import com.peknight.codec.configuration.CodecConfiguration
+import com.peknight.codec.cursor.Cursor
+import com.peknight.codec.http4s.instances.status.given
+import com.peknight.codec.sum.*
+import com.peknight.codec.{Codec, Decoder, Encoder}
 import io.circe.JsonObject
 import org.http4s.Status
 
@@ -15,5 +21,10 @@ case class AccountDoesNotExist(detail: String,
   def description: String = "The request specified an account that does not exist"
 end AccountDoesNotExist
 object AccountDoesNotExist:
-  
+  given codecAccountDoesNotExist[F[_], S](using Monad[F], ObjectType[S], NullType[S], ArrayType[S], BooleanType[S],
+                                          NumberType[S], StringType[S], Encoder[F, S, JsonObject],
+                                          Decoder[F, Cursor[S], JsonObject]
+                                         ): Codec[F, S, Cursor[S], AccountDoesNotExist] =
+    given CodecConfiguration = ACMEServerError.codecConfiguration
+    Codec.derived[F, S, AccountDoesNotExist]
 end AccountDoesNotExist
--- acme-core/shared/src/main/scala/com/peknight/acme/identifier/Identifier.scala
+++ acme-core/shared/src/main/scala/com/peknight/acme/identifier/Identifier.scala
@@ -1,5 +1,19 @@
 package com.peknight.acme.identifier
 
+import cats.Monad
+import com.peknight.codec.circe.iso.codec
+import com.peknight.codec.circe.sum.jsonType.given
+import com.peknight.codec.configuration.given
+import com.peknight.codec.cursor.Cursor
+import com.peknight.codec.sum.{NullType, ObjectType, StringType}
+import com.peknight.codec.{Codec, Decoder, Encoder}
+import io.circe.Json
+
 case class Identifier(`type`: IdentifierType, value: String)
 object Identifier:
+  given codecIdentifier[F[_], S](using Monad[F], ObjectType[S], NullType[S], StringType[S]): Codec[F, S, Cursor[S], Identifier] =
+    Codec.derived[F, S, Identifier]
+  given jsonCodecIdentifier[F[_]: Monad]: Codec[F, Json, Cursor[Json], Identifier] =
+    codecIdentifier[F, Json]
+  given circeCodecIdentifier: io.circe.Codec[Identifier] = codec[Identifier]
 end Identifier
