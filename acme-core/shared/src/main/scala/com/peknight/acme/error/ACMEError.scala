12673963e37ed21957668082728069f6662b342e
peknight <JKpeknight@gmail.com>
2025-02-15 14:36:09 +0800

    update

--- acme-core/shared/src/main/scala/com/peknight/acme/error/ACMEError.scala
+++ acme-core/shared/src/main/scala/com/peknight/acme/error/ACMEError.scala
@@ -1,7 +1,15 @@
 package com.peknight.acme.error
 
+import cats.Monad
 import com.peknight.acme.identifier.Identifier
+import com.peknight.codec.circe.iso.codec
+import com.peknight.codec.circe.sum.jsonType.given
+import com.peknight.codec.cursor.Cursor
+import com.peknight.codec.error.NoSuchEnum
+import com.peknight.codec.sum.{ArrayType, NullType, ObjectType, StringType}
+import com.peknight.codec.{Codec, Decoder, Encoder}
 import com.peknight.error.Error
+import io.circe.Json
 
 case class ACMEError(
                       acmeErrorType: ACMEErrorType,
@@ -15,4 +23,18 @@ case class ACMEError(
 end ACMEError
 object ACMEError:
   def `type`(acmeErrorType: ACMEErrorType): String = s"urn:ietf:params:acme:error:$acmeErrorType"
+
+  given codecACMEError[F[_], S](using Monad[F], ObjectType[S], NullType[S], ArrayType[S], StringType[S])
+  : Codec[F, S, Cursor[S], ACMEError] =
+    Codec.forProduct[F, S, ACMEError, (String, String, Option[Identifier], Option[List[ACMEError]])]
+      (("type", "detail", "identifier", "subproblems"))(acmeError =>
+        (acmeError.`type`, acmeError.detail, acmeError.identifier, acmeError.subProblems)
+      ) { (typ, detail, identifier, subProblems) =>
+        ACMEErrorType.values.find(errorType => `type`(errorType) == typ)
+          .map(errorType => ACMEError(errorType, detail, identifier, subProblems))
+          .toRight(NoSuchEnum(typ))
+      }
+  given jsonCodecACMEError[F[_]: Monad]: Codec[F, Json, Cursor[Json], ACMEError] =
+    codecACMEError[F, Json]
+  given circeCodecACMEError: io.circe.Codec[ACMEError] = codec[ACMEError]
 end ACMEError
