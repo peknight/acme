d1a427b0e18c40d9fa3b9b3a9cc45e43ccc91563
peknight <JKpeknight@gmail.com>
2025-02-21 19:55:31 +0800

    update

--- acme-core/shared/src/main/scala/com/peknight/acme/order/OrderClaims.scala
+++ acme-core/shared/src/main/scala/com/peknight/acme/order/OrderClaims.scala
@@ -1,6 +1,14 @@
 package com.peknight.acme.order
 
+import cats.Monad
 import com.peknight.acme.identifier.Identifier
+import com.peknight.codec.circe.iso.codec
+import com.peknight.codec.circe.sum.jsonType.given
+import com.peknight.codec.configuration.CodecConfiguration
+import com.peknight.codec.cursor.Cursor
+import com.peknight.codec.sum.*
+import com.peknight.codec.{Codec, Decoder, Encoder}
+import io.circe.{Json, JsonObject}
 
 import java.time.Instant
 
@@ -12,3 +20,19 @@ case class OrderClaims(
                         replaces: Option[String] = None,
                         profile: Option[String] = None,
                       )
+object OrderClaims:
+  private val memberNameMap: Map[String, String] = Map(
+    "autoRenewal" -> "auto-renewal"
+  )
+  given codecOrderClaims[F[_], S](using Monad[F], ObjectType[S], NullType[S], ArrayType[S], BooleanType[S],
+                                    NumberType[S], StringType[S], Encoder[F, S, JsonObject],
+                                    Decoder[F, Cursor[S], JsonObject]): Codec[F, S, Cursor[S], OrderClaims] =
+    given CodecConfiguration = CodecConfiguration.default
+      .withTransformMemberNames(memberName => memberNameMap.getOrElse(memberName, memberName))
+      .withExtField("ext")
+    Codec.derived[F, S, OrderClaims]
+
+  given jsonCodecOrderClaims[F[_]: Monad]: Codec[F, Json, Cursor[Json], OrderClaims] = codecOrderClaims[F, Json]
+
+  given circeCodecOrderClaims: io.circe.Codec[OrderClaims] = codec[OrderClaims]
+end OrderClaims
