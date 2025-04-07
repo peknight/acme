70af4196d16b465a35be1791c6c7f3c923034185
peknight <JKpeknight@gmail.com>
2025-04-07 14:55:48 +0800

    update

--- acme-client/http4s/jvm/src/test/scala/com/peknight/acme/client/http4s/ACMEApiFlatSpec.scala
+++ acme-client/http4s/jvm/src/test/scala/com/peknight/acme/client/http4s/ACMEApiFlatSpec.scala
@@ -6,12 +6,12 @@ import cats.effect.testing.scalatest.AsyncIOSpec
 import cats.syntax.applicative.*
 import cats.syntax.either.*
 import cats.syntax.functor.*
+import cats.syntax.option.*
 import cats.{Id, Show}
 import com.comcast.ip4s.port
 import com.peknight.acme.challenge.Challenge.`dns-01`
 import com.peknight.acme.client.cloudflare.DNSChallengeClient
 import com.peknight.acme.client.letsencrypt.challenge.Challenge
-import com.peknight.acme.client.letsencrypt.uri
 import com.peknight.acme.client.letsencrypt.uri.stagingDirectory
 import com.peknight.acme.identifier.Identifier
 import com.peknight.acme.identifier.Identifier.DNS
@@ -20,7 +20,6 @@ import com.peknight.cloudflare.dns.record.DNSRecordId
 import com.peknight.cloudflare.dns.record.http4s.DNSRecordApi
 import com.peknight.cloudflare.test.{pekToken, pekZoneId}
 import com.peknight.codec.syntax.encoder.asS
-import com.peknight.error.std.JavaThrowable
 import com.peknight.error.syntax.applicativeError.asError
 import com.peknight.jose.jwk.JsonWebKey
 import com.peknight.logging.syntax.eitherT.log
@@ -30,24 +29,16 @@ import com.peknight.security.bouncycastle.openssl.{fetchKeyPair, fetchX509Certif
 import com.peknight.security.cipher.RSA
 import com.peknight.security.ecc.sec.secp256r1
 import com.peknight.security.key.store.pkcs12
-import com.peknight.security.syntax.keyStore.storePath
 import fs2.io.file.Path
-import fs2.io.net.Network
 import io.circe.Json
-import org.bouncycastle.asn1.ASN1ObjectIdentifier
 import org.http4s.*
 import org.http4s.client.dsl
-import org.http4s.dsl.io.*
 import org.http4s.ember.client.EmberClientBuilder
-import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.middleware.Logger as MiddlewareLogger
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
-import java.security.cert.X509Certificate
-import java.security.{KeyPair, Provider}
-import javax.security.auth.x500.X500Principal
+import java.security.KeyPair
 import scala.concurrent.duration.*
 
 class ACMEApiFlatSpec extends AsyncFlatSpec with AsyncIOSpec:
@@ -72,10 +63,11 @@ class ACMEApiFlatSpec extends AsyncFlatSpec with AsyncIOSpec:
                 given DNSRecordApi[IO] = DNSRecordApi[IO](pekToken)(client)(dsl.io)
                 dnsChallengeClient <- EitherT(DNSChallengeClient[IO](pekZoneId).asError)
                 accountKeyPair <- EitherT(fetchKeyPair[IO](Path("cert/account.key"))(
-                  secp256r1.generateKeyPair[IO](provider = Some(provider)).asError)).log(name = "accountKeyPair")
+                  secp256r1.generateKeyPair[IO](provider = provider.some).asError)).log(name = "accountKeyPair")
                 domainKeyPair <- EitherT(fetchKeyPair[IO](Path("cert/domain.key"))(
                   RSA.keySizeGenerateKeyPair[IO](4096).asError)).log(name = "domainKeyPair")
-                certificates <- EitherT(fetchX509Certificates[IO](Path("cert/domain-chain.crt"), provider = Some(provider)) {
+                certificates <- EitherT(fetchX509Certificates[IO](Path("cert/domain-chain.crt"),
+                  provider = provider.some) {
                   val et =
                     for
                       identifiers <- NonEmptyList.of(
@@ -94,52 +86,18 @@ class ACMEApiFlatSpec extends AsyncFlatSpec with AsyncIOSpec:
                         dnsChallengeClient.createDNSRecord
                       )(
                         dnsChallengeClient.cleanDNSRecord(_, _, _).map(_.as(()))
-                      )).log(name = "fetchCertificates", param = Some(identifiers))
+                      )).log(name = "fetchCertificates", param = identifiers.some)
                     yield
                       certificates.certificates
                   et.value
                 }).log(name = "certificates")
-//                keyStore <- EitherT(pkcs12[IO]("", domainKeyPair.getPrivate, "", certificates,
-//                  Some(provider)).asError).log(name = "keyStore")
-                keyStore <- EitherT(buildKeyStore(domainKeyPair, certificates, None).asError)
-//                _ <- EitherT(keyStore.storePath[IO](Path("cert/domain.p12"), "abcdef").asError).log(name = "storePath")
-//                keyStore <- EitherT(pkcs12.loadPath[IO](Path("cert/domain.p12"), "abcdef", Some(provider)).asError)
-//                  .log(name = "loadPath")
-//                tlsContext <- EitherT(Network[IO].tlsContext.fromKeyStoreFile(Path("cert/domain-openssl.p12").toNioPath, "123456".toCharArray, "123456".toCharArray).asError)
-                tlsContext <- EitherT(Network[IO].tlsContext.fromKeyStore(keyStore, "123456".toCharArray).asError)
-                res <- EitherT(EmberServerBuilder.default[IO]
-                  .withHostOption(None)
-                  .withPort(port"8080")
-                  .withTLS(tlsContext)
-                  .withHttpApp(MiddlewareLogger.httpApp(true, false)(HttpRoutes.of[IO] {
-                    case req => Ok("rua")
-                  }.orNotFound))
-                  .build.allocated
-                  .asError)
-                _ <- EitherT(IO.never.asError)
+                keyStore <- EitherT(pkcs12[IO]("", domainKeyPair.getPrivate, "", certificates).asError)
+                  .log(name = "keyStore")
               yield
                 ()
             eitherT.value
           }
       yield either
-    run.asserting(either =>
-      println(either.left.map(error => error.asInstanceOf[JavaThrowable[Throwable]].error.printStackTrace()))
-      assert(either.isRight))
+    run.asserting(either => assert(either.isRight))
   }
-
-  private def buildKeyStore(keyPair: KeyPair, chain: NonEmptyList[java.security.cert.Certificate], provider: Option[Provider | java.security.Provider] = None): IO[java.security.KeyStore] =
-    val certChain = chain.toList.toArray
-    given CanEqual[Any, Any] = CanEqual.derived
-    for (i <- 0 until certChain.length - 1) {
-      val issuerDN = certChain(i).asInstanceOf[X509Certificate].getIssuerX500Principal
-      val subjectDN = certChain(i + 1).asInstanceOf[X509Certificate].getSubjectX500Principal
-      println(s"[iss]: $issuerDN [sub]: $subjectDN")
-      if (!(issuerDN == (subjectDN))) println("mie")
-    }
-    for
-      keyStore <- pkcs12.getKeyStore[IO](provider)
-      _ <- IO(keyStore.load(null, null))
-      _ <- IO(keyStore.setKeyEntry("", keyPair.getPrivate, "123456".toCharArray, chain.toList.toArray))
-    yield
-      keyStore
 end ACMEApiFlatSpec
