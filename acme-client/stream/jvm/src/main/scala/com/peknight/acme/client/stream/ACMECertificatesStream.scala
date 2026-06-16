08c42ab0db843ac21fd388fcc87da4a241d5c7e9
peknight <JKpeknight@gmail.com>
2026-06-16 16:10:56 +0800

    feat: add persisted acme certificate stream

--- acme-client/stream/jvm/src/main/scala/com/peknight/acme/client/stream/ACMECertificatesStream.scala
+++ acme-client/stream/jvm/src/main/scala/com/peknight/acme/client/stream/ACMECertificatesStream.scala
@@ -4,6 +4,7 @@ import cats.Applicative
 import cats.data.NonEmptyList
 import cats.effect.*
 import cats.syntax.applicative.*
+import cats.syntax.either.*
 import cats.syntax.flatMap.*
 import cats.syntax.functor.*
 import cats.syntax.option.*
@@ -16,9 +17,12 @@ import com.peknight.cats.instances.instant.given
 import com.peknight.commons.time.syntax.temporal.-
 import com.peknight.error.Error
 import com.peknight.fs2.stream.unfoldTemporal
+import com.peknight.security.bouncycastle.openssl.{fetchKeyPair, readX509CertificatesAndKeyPair, writeX509CertificatesAndKeyPair}
+import com.peknight.security.provider.Provider
 import fs2.Stream
+import fs2.io.file.{Files, Path}
 
-import java.security.KeyPair
+import java.security.{KeyPair, Provider as JProvider}
 import java.security.cert.X509Certificate
 import scala.concurrent.duration.*
 
@@ -46,6 +50,65 @@ object ACMECertificatesStream:
     )
   end apply
 
+  def persisted[F[_], Challenge <: ACMEChallenge, I <: Identifier, Child <: ACMEChallenge, Record](
+    config: IssueConfig,
+    accountKeyPath: Path,
+    domainKeyPath: Path,
+    certificatePath: Path,
+    renewalWindow: FiniteDuration = 7.days,
+    issueRetryInterval: FiniteDuration = 1.hour,
+    certProvider: Option[Provider | JProvider] = None,
+    keyProvider: Option[Provider | JProvider] = None,
+  )(
+    accountKeyPairSource: F[Either[Error, KeyPair]],
+    domainKeyPairSource: F[Either[Error, KeyPair]],
+  )(
+    using
+    acmeClient: ACMEClient[F, Challenge], challengeClient: ChallengeClient[F, Challenge, I, Child, Record],
+    async: Async[F], files: Files[F]
+  ): Stream[F, (NonEmptyList[X509Certificate], KeyPair)] =
+    Stream.eval(fetchKeyPair[F](accountKeyPath, keyProvider)(accountKeyPairSource).flatMap {
+      case Right(accountKeyPair) => accountKeyPair.pure[F]
+      case Left(error) => Sync[F].raiseError[KeyPair](error)
+    }).flatMap { accountKeyPair =>
+      val acmeStream = apply[F, Challenge, I, Child, Record, Unit](config, renewalWindow, issueRetryInterval)(
+        accountKeyPair.asRight[Error].pure[F],
+        domainKeyPairSource,
+      ).evalTap { case (certificates, domainKeyPair) =>
+        writeX509CertificatesAndKeyPair[F](certificatePath, domainKeyPath)(certificates, domainKeyPair).flatMap {
+          case Right(()) => ().pure[F]
+          case Left(error) => Sync[F].raiseError[Unit](error)
+        }
+      }
+      Stream.eval(readLocalCertificatesAndKeyPair[F](certificatePath, domainKeyPath, certProvider, keyProvider)).flatMap {
+        case Some((certificates, domainKeyPair)) =>
+          Stream.emit((certificates, domainKeyPair)) ++
+            Stream.eval(interval[F](certificates, renewalWindow, issueRetryInterval)).flatMap {
+              case Some(duration) => Stream.sleep_[F](duration)
+              case None => Stream.empty
+            } ++ acmeStream
+        case None => acmeStream
+      }
+    }
+  end persisted
+
+  private def readLocalCertificatesAndKeyPair[F[_]](
+    certificatePath: Path,
+    domainKeyPath: Path,
+    certProvider: Option[Provider | JProvider],
+    keyProvider: Option[Provider | JProvider],
+  )(using Sync[F], Clock[F], Files[F]): F[Option[(NonEmptyList[X509Certificate], KeyPair)]] =
+    readX509CertificatesAndKeyPair[F](certificatePath, domainKeyPath, certProvider, keyProvider).flatMap {
+      case Left(error) => Sync[F].raiseError[Option[(NonEmptyList[X509Certificate], KeyPair)]](error)
+      case Right(Some(value @ (certificates, _))) =>
+        notExpired[F](certificates).map(valid => Option.when(valid)(value))
+      case Right(None) => none[(NonEmptyList[X509Certificate], KeyPair)].pure[F]
+    }
+
+  private def notExpired[F[_]: {Applicative, Clock}](certificates: NonEmptyList[X509Certificate]): F[Boolean] =
+    Option(certificates.head.getNotAfter) match
+      case Some(notAfter) => Clock[F].realTimeInstant.map(_ < notAfter.toInstant)
+      case _ => false.pure[F]
 
   private def interval[F[_]: {Applicative, Clock}](certificates: NonEmptyList[X509Certificate],
                                                    threshold: FiniteDuration, retryInterval: FiniteDuration)
